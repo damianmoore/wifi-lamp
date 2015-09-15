565257f6c7ca9e282c698c7c95ce73b1c54a84b5
Damian Moore <damian@epixstudios.co.uk>
2015-09-16 00:19:12 +0100

    Light dimming controlled by HTML range slider

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 function getUrl(url) {
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', url, true);
+  xmlhttp.open('GET', url, false);
   xmlhttp.send();
 };
 
 
-var app = document.getElementById('app');
+var WifiLampApp = React.createClass({
+  getInitialState: function() {
+    var state = {
+      brightness: configVars.brightness,
+    };
+    return state;
+  },
+  setBrightness: function(val) {
+    getUrl('/api/?val=' + val);
+    this.setState({brightness: val});
+  },
+  handleRangeChange: function(e) {
+    this.setBrightness(e.currentTarget.valueAsNumber);
+  },
+  render: function() {
+    var rangeButtons = [];
+    for (var i = 0; i <= 10; i++) {
+      rangeButtons.push(<button type="button" onClick={this.setBrightness.bind(null, i / 10)}>{i}0%</button>);
+    }
+    return (
+      <div>
+        <h1>Wi-Fi Lamp</h1>
+        <p>Uptime: {configVars.uptime}</p>
+        <p>
+          <button type="button" onClick={this.setBrightness.bind(null, 0)}>OFF</button>
+          <button type="button" onClick={this.setBrightness.bind(null, 1)}>ON</button>
+        </p>
+        <p>
+          {rangeButtons}
+        </p>
+        <p>
+          <input id="slide" type="range" min="0" max="1.0" step="0.001" defaultValue={this.state.brightness} value={this.state.brightness} onChange={this.handleRangeChange} />
+        </p>
+        <p>State: {this.state.brightness}</p>
+      </div>
+    );
+  }
+});
+
 
 React.render(
-  (
-    <div>
-      <h1>Wi-Fi Lamp</h1>
-      <p>Uptime: {configVars.uptime}</p>
-      <p>
-        <button type="button" onClick={getUrl.bind(null, '/on')}>ON</button>
-        <button type="button" onClick={getUrl.bind(null, '/off')}>OFF</button>
-      </p>
-      <p>State: {configVars.lightState}</p>
-    </div>
-  ),
-  app
+  <WifiLampApp />,
+  document.getElementById('app')
 );
